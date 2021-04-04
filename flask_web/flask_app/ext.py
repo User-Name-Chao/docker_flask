fca225290503837df779eaf0f702516155e4358d
User-Name-Chao <1371607399@qq.com>
2021-04-04 17:54:34 +0800

    Update ext.py

--- flask_web/flask_app/ext.py
+++ flask_web/flask_app/ext.py
@@ -1,11 +1,8 @@
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect
 from flask_cors import CORS
 
 from flask_app.models import db
-
-csrf = CSRFProtect()
 
 migrate = Migrate(db=db)
 
@@ -17,4 +14,3 @@ def init_ext(app):
     migrate.init_app(app)
     CORS(app, supports_credentials=True)
     login_manager.init_app(app)
-    csrf.init_app(app)
